8db2fd2f055a16e654c179c4952f545d0e7163d0
ASNOUN <samir.asnoun@gmail.com>
2017-03-07 09:08:08 +0100

    adding urls

--- Bethnoe/EgliseBethnoe/urls.py
+++ Bethnoe/EgliseBethnoe/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.contrib import admin
- 
+  
 
 urlpatterns = [
     url(r'^$', views.IndexView, name="index"),
